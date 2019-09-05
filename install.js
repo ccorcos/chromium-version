4d8087d2487de11016f4faf3891417c66e077adb
taku.omi <taku.omi@gree.co.jp>
2019-09-05 18:52:27 +0900

    update version.

--- install.js
+++ install.js
@@ -70,7 +70,7 @@ async function install() {
         //console.info('Step 1. Retrieving Chromium latest revision number');
         //const revision = await utils.getLatestRevisionNumber();
         console.info('Step 1. Chromium revision is 76.');
-        const revision = '76.0.3809.132';
+        const revision = '665002';
 
         console.info('Step 2. Downloading Chromium (this might take a while)');
         const tmpPath = await downloadChromiumRevision(revision);
